eb195aa3cb3bb0b0105cb6edba9ff16e29dda84f
voccer <ntduc250798@gmail.com>
2021-12-06 01:47:45 +0700

    change name

--- ecr_test/app.py
+++ ecr_test/app.py
@@ -2,12 +2,12 @@ import json
 
 
 def lambda_handler(event, context):
-  
+
     return {
         "statusCode": 200,
         "body": json.dumps(
             {
-                "message": "hello world",
+                "message": "hello world123",
             }
         ),
     }
